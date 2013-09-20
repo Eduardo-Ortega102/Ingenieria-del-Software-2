57d185f2354aab8ba600d3019705cd10f895f0d9
ASUS <ASUS@ASUS-PC>
2013-09-20 12:17:57 +0100

    Second commit (with 3 system.out.println)

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -8,7 +8,8 @@ public class Kata1 {
     public static void main(String[] args) {
         Student ed = new Student("Eduardo", new Date(93, 9, 13));
         
-        System.out.println("name: " + ed.getName() + 
-                "\nbirthday: " + ed.getBirthday() + "\nage: " + ed.getAge());
+        System.out.println("name: " + ed.getName());
+        System.out.println("birthday: " + ed.getBirthday());
+        System.out.println("age: " + ed.getAge());
     }
 }
